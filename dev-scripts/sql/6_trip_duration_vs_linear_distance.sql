5fdf975b7425a0049714020aa60fb5108f53bed3
Alex Yeo <ay@4ei.co.uk>
2022-07-29 18:20:01 +0100

    more sql

--- dev-scripts/sql/6_trip_duration_vs_linear_distance.sql
+++ dev-scripts/sql/6_trip_duration_vs_linear_distance.sql
@@ -8,3 +8,47 @@ SELECT corr(
         ST_Transform(tbl.dropoff_point, 3626)
     )
 ) FROM journeys_spatial AS tbl;
+
+
+-- Average distance for grouped trip durations.
+--
+--
+-- Selecting the discrete trip duration class, and the average distance travelled by a taxi journey
+-- within that trip duration class, from a table containing the pickup and dropoff points and the
+-- categorised trip duration values.
+--
+-- Simplified: 
+--      SELECT trip_duration_class, average_distance 
+--      FROM tbl_points_and_trip_duration_class
+--      GROUP BY trip_duration_class;
+--
+-- The 'tbl_points_and_trip_duration_class' is created by selecting from the original table and using
+-- a CASE statement to group the trip_duration values and relable them.
+
+
+SELECT  
+
+d.trip_duration_class,   
+AVG(
+    ST_Distance(
+        ST_Transform(d.pickup_point, 3626),
+        ST_Transform(d.dropoff_point, 3626)
+    )
+) AS average_distance
+
+FROM (
+    SELECT 
+    tbl.pickup_point,
+    tbl.dropoff_point,
+    (
+        CASE 
+            WHEN tbl.trip_duration BETWEEN 0 AND 1000000 THEN 1
+            WHEN tbl.trip_duration BETWEEN 1000000 AND 2000000 THEN 2
+            WHEN tbl.trip_duration BETWEEN 2000000 AND 3000000 THEN 3
+            ELSE 4
+        END
+    ) AS trip_duration_class
+    FROM journeys_spatial AS tbl
+) AS d
+
+GROUP BY d.trip_duration_class;
